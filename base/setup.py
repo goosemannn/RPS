669d8c5302b567a9e1fca54c88ea7268415195ec
Brandon <66843207+goosemannn@users.noreply.github.com>
2023-04-05 21:16:30 +0000

    initial

--- base/setup.py
+++ base/setup.py
@@ -4,7 +4,7 @@ import sys
 with open('./game.py', 'w') as f:
   with open('./base/game.py.txt', 'r') as f2:
     baseGame = f2.read()
-    gameCode = baseGamer.replace('%GAMEFOLDER%', sys.argv[2])
+    gameCode = baseGame.replace('%GAMEFOLDER%', sys.argv[2])
     f.write(gameCode)
     
 
