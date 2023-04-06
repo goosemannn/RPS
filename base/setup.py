a4b0c7c306f691a50343f4633098be09748ea8a3
Brandon <66843207+goosemannn@users.noreply.github.com>
2023-04-06 15:56:31 +0000

    Worked on setup

--- base/setup.py
+++ base/setup.py
@@ -1,10 +1,34 @@
 import os
 import sys
+import random
 
-with open('./game.py', 'w') as f:
+
+os.mkdir('./games/' + sys.argv[1])
+
+with open(f'./game/{sys.argv[1]}/game.py', 'w') as f:
   with open('./base/game.py.txt', 'r') as f2:
     baseGame = f2.read()
-    gameCode = baseGame.replace('%GAMEFOLDER%', sys.argv[2])
+    gameCode = baseGame.replace('%GAMEFOLDER%', sys.argv[1])
     f.write(gameCode)
+
+with open(f'./game/{sys.argv[1]}/bot1.py', 'w') as f:
+  with open('./base/bot1.py.txt', 'r') as f2:
+    rockList = ["r" for x in range(0, random.randint(1, 20))]
+    paperList = ["p" for x in range(0, random.randint(1, 20))]
+    scissorsList = ["s" for x in range(0, random.randint(1, 20))]
+    l = rockList + paperList + scissorsList 
+    baseSetup = f2.read()
+    setupCode = baseSetup.replace('%LIST%', str(l))
+    f.write(setupCode)
+
+with open(f'./game/{sys.argv[1]}/bot2.py', 'w') as f:
+  with open('./base/bot2.py.txt', 'r') as f2:
+    rockList = ["r" for x in range(0, random.randint(1, 20))]
+    paperList = ["p" for x in range(0, random.randint(1, 20))]
+    scissorsList = ["s" for x in range(0, random.randint(1, 20))]
+    l = rockList + paperList + scissorsList 
+    baseSetup = f2.read()
+    setupCode = baseSetup.replace('%LIST%', str(l))
+    f.write(setupCode)
     
 
